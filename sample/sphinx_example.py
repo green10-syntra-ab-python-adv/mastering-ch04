b3606e3a9edcb74bae96fca05a1cc4623fef26e3
HansVdb <hans.vandenbogaerde@gmail.com>
2022-02-15 15:54:07 +0100

    aanpassing tbv les van 21/2/2022

---
+++ sample/sphinx_example.py
@@ -0,0 +1,23 @@
+def docstring_example(foo, bar):
+    """Munge foo and bar
+
+    :param foo: should be an instance fo :class: 'Twingy' or
+        have a method equivalent to :meth: 'Twingy.munge'
+    :param bar: will be passed as first parameter to :meth: 'Twingy.munge'
+    :return:
+    """
+    foo.munge(bar)
+
+
+def tatata(ata, tat):
+    """
+
+    :param ata:
+    :param tat:
+    :return:
+    """
+
+def atatat(aaa, bbb):
+    """
+
+    """
